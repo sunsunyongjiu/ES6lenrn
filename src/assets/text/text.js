5d1d2b636bff35d065c3cfb411ca182f8380252a
sunshiyi <shiyi.sun@htwins.com.cn>
2017-08-07 13:35:55 +0800

    no message

--- src/assets/text/text.js
+++ src/assets/text/text.js
@@ -6,7 +6,9 @@ const notes ={
 		{title:'angular2-4',link:'http://www.cnblogs.com/yitim/category/949322.html'},
 		{title:'tguide',link:'http://tguide.qq.com/main/index.htm'},
 		{title:'es6入门第二版',link:'http://es6.ruanyifeng.com/'},
-		{title:'nav内凹圆角',link:'http://www.tuicool.com/articles/BFbIzue'}
+		{title:'nav内凹圆角',link:'http://www.tuicool.com/articles/BFbIzue'},
+		{title:'skew倾斜',link:'http://www.cnblogs.com/mm2015/p/4812763.html'},
+		{title:'30分钟入门es6',link:'http://www.jianshu.com/p/ebfeb687eb70'},
 	]
 }
 export default notes
--- src/pages/test.vue
+++ src/pages/test.vue
@@ -1,28 +1,54 @@
 <template>
-    <div>
-        <my-nav></my-nav>
+  <div>
+    <my-nav></my-nav>
+    <div v-show-me class="show">
     </div>
+  </div>
 </template>
 <script>
 import myNav from '../components/nav'
-    export default {
-        data () {
-            return {
-                msg: "我是home 组件"
-            }
-        },
-        components:{
-            myNav
-        },
-        methods:{
-
-        },
-        mouted:function(){
-            console.log(11111111111)
-        }
+export default {
+  data() {
+    return {
+      msg: "我是home 组件"
     }
+  },
+  components: {
+    myNav
+  },
+  methods: {
+
+  },
+  directives: {
+    showMe(el) {
+      let oDiv = el; //el --> 触发的DOM元素
+      oDiv.onmousedown = function(e) {
+        console.log(e)
+        let l = e.clientX - oDiv.offsetLeft;
+        let t = e.clientY - oDiv.offsetTop;
+        document.onmousemove = function(e) {
+          oDiv.style.left = e.clientX - l + 'px';
+          oDiv.style.top = e.clientY - t + 'px';
+        };
+        oDiv.onmouseup = function() {
+          document.onmousemove = null;
+          oDiv.onmouseup = null;
+        }
+      }
+    }
+  },
+  mouted: function() {
+    console.log(11111111111)
+  }
+}
+
 </script>
 <style lang="less">
-    @import '../assets/css/Es6.less';
-
+@import '../assets/css/Es6.less';
+.show{
+    width: 20vw;
+    height: 20vw;
+    background: yellow;
+    position: absolute;
+}
 </style>
