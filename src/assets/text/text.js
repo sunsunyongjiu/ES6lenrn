c089ae9f66eb6f4638dc8b12ddcc001d6a6eea94
sunshiyi <shiyi.sun@htwins.com.cn>
2017-09-15 14:05:01 +0800

    no message

--- src/assets/text/text.js
+++ src/assets/text/text.js
@@ -1,16 +1,17 @@
-const notes ={
-	notes:[
-		{title:'IIFE',text:'立即执行函数表达式'},
-		{title:'webpack学习',link:'http://www.w2bc.com/Article/50764'},
-		{title:'前端规范等其他前端知识',link:'http://www.css88.com/'},
-		{title:'angular2-4',link:'http://www.cnblogs.com/yitim/category/949322.html'},
-		{title:'tguide',link:'http://tguide.qq.com/main/index.htm'},
-		{title:'es6入门第二版',link:'http://es6.ruanyifeng.com/'},
-		{title:'nav内凹圆角',link:'http://www.tuicool.com/articles/BFbIzue'},
-		{title:'skew倾斜',link:'http://www.cnblogs.com/mm2015/p/4812763.html'},
-		{title:'30分钟入门es6',link:'http://www.jianshu.com/p/ebfeb687eb70'},
-		{title:'vue-directive',link:'http://www.jb51.net/article/108047.htm'},
-		{title:'手机端client undefined解决方案',link:'http://blog.csdn.net/zzxboy1/article/details/65626605'},
-	]
+const notes = {
+  notes: [
+    { title: 'IIFE', text: '立即执行函数表达式' },
+    { title: 'webpack学习', link: 'http://www.w2bc.com/Article/50764' },
+    { title: '前端规范等其他前端知识', link: 'http://www.css88.com/' },
+    { title: 'angular2-4', link: 'http://www.cnblogs.com/yitim/category/949322.html' },
+    { title: 'tguide', link: 'http://tguide.qq.com/main/index.htm' },
+    { title: 'es6入门第二版', link: 'http://es6.ruanyifeng.com/' },
+    { title: 'nav内凹圆角', link: 'http://www.tuicool.com/articles/BFbIzue' },
+    { title: 'skew倾斜', link: 'http://www.cnblogs.com/mm2015/p/4812763.html' },
+    { title: '30分钟入门es6', link: 'http://www.jianshu.com/p/ebfeb687eb70' },
+    { title: 'vue-directive', link: 'http://www.jb51.net/article/108047.htm' },
+    { title: '手机端client undefined解决方案', link: 'http://blog.csdn.net/zzxboy1/article/details/65626605' },
+    { title: 'angular2 大漠穷秋', link: 'http://blog.csdn.net/enterlind/article/details/60874006' },
+  ]
 }
 export default notes
--- src/pages/test.vue
+++ src/pages/test.vue
@@ -3,18 +3,48 @@
     <my-nav></my-nav>
     <div v-show-me class="show">
     </div>
+    <group title="chained columns">
+      <popup-picker :data="list3" :columns="4" v-model="value4" show-name :column-width="width"></popup-picker>
+    </group>
   </div>
 </template>
 <script>
 import myNav from '../components/nav'
+import { PopupPicker, Group } from 'vux'
 export default {
   data() {
     return {
-      msg: "我是home 组件"
+      msg: "我是home 组件",
+      value4: [],
+      width:[1/4,1/4,1/4],
+      list3: [{
+          name: '中国',
+          value: 'china',
+          parent: 0
+        },{
+          name: '中国1',
+          value: 'china1',
+          parent: 'china'
+        },
+        {
+          name: '中国2',
+          value: 'china2',
+          parent: 'china1'
+        },
+        {
+          name: '中国3',
+          value: 'china3',
+          parent: 'china2'
+        }
+
+
+      ],
     }
   },
   components: {
-    myNav
+    myNav,
+    PopupPicker,
+    Group
   },
   methods: {
 
@@ -45,10 +75,11 @@ export default {
 </script>
 <style lang="less">
 @import '../assets/css/Es6.less';
-.show{
-    width: 20vw;
-    height: 20vw;
-    background: yellow;
-    position: absolute;
+.show {
+  width: 20vw;
+  height: 20vw;
+  background: yellow;
+  position: absolute;
 }
+
 </style>
