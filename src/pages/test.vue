90bcb77dc8d009a89827d396b72e90d22c271775
sunshiyi <shiyi.sun@htwins.com.cn>
2017-09-19 09:41:44 +0800

    no message

--- src/pages/test.vue
+++ src/pages/test.vue
@@ -1,85 +1,96 @@
 <template>
-  <div>
-    <my-nav></my-nav>
-    <div v-show-me class="show">
+  <div class="vue-upload-img-multiple">
+    <div v-if="images.length >0">
+      <ul>
+        <li v-for="image in images">
+          <img :src="image" @click='delImage($index)' />
+          <a href="#" style="position: absolute;" @click='delImage($index)'>
+            <span class="glyphicon glyphicon-remove"></span>
+          </a>
+        </li>
+      </ul>
+      <button @click="removeImage">移除全部图片</button>
+      <button @click='addPic' >上传</button>
     </div>
-    <group title="chained columns">
-      <popup-picker :data="list3" :columns="4" v-model="value4" show-name :column-width="width"></popup-picker>
-    </group>
+
+    <div>
+      <div v-if="!image">
+        <h2>Select an image</h2>
+        <input type="file" @change="onFileChange">
+      </div>
+      <div v-else>
+        <img :src="image" />
+        <button @click="removeImage">Remove image</button>
+      </div>
+    </div>
+
   </div>
 </template>
+
 <script>
-import myNav from '../components/nav'
-import { PopupPicker, Group } from 'vux'
 export default {
-  data() {
+  name: 'Upload',
+  data: function () {
     return {
-      msg: "我是home 组件",
-      value4: [],
-      width:[1/4,1/4,1/4],
-      list3: [{
-          name: '中国',
-          value: 'china',
-          parent: 0
-        },{
-          name: '中国1',
-          value: 'china1',
-          parent: 'china'
-        },
-        {
-          name: '中国2',
-          value: 'china2',
-          parent: 'china1'
-        },
-        {
-          name: '中国3',
-          value: 'china3',
-          parent: 'china2'
-        }
-
-
-      ],
+      images: []
     }
-  },
-  components: {
-    myNav,
-    PopupPicker,
-    Group
   },
   methods: {
-
-  },
-  directives: {
-    showMe(el) {
-      let oDiv = el; //el --> 触发的DOM元素
-      oDiv.onmousedown = function(e) {
-        console.log(e)
-        let l = e.clientX - oDiv.offsetLeft;
-        let t = e.clientY - oDiv.offsetTop;
-        document.onmousemove = function(e) {
-          oDiv.style.left = e.clientX - l + 'px';
-          oDiv.style.top = e.clientY - t + 'px';
-        };
-        oDiv.onmouseup = function() {
-          document.onmousemove = null;
-          oDiv.onmouseup = null;
+    test () {
+      var vm = this
+      console.log(vm.message)
+    },
+    addPic () {
+      $('input[type=file]').trigger('click')
+      return false
+    },
+    onFileChange (e) {
+      var files = e.target.files || e.dataTransfer.files
+      if (!files.length) return
+      this.createImage(files)
+    },
+    createImage (file) {
+      var vm = this
+      var reader = null
+      var leng = file.length
+      for (var i = 0; i < leng; i++) {
+        reader = new window.FileReader()
+        reader.readAsDataURL(file[i])
+        reader.onload = function (e) {
+          vm.images.push(e.target.result)
+          console.log(e.target.result)
         }
       }
+    },
+    removeImage: function (e) {
+      this.images = []
+    },
+    delImage: function (index) {
+      this.images.shift(index)
     }
-  },
-  mouted: function() {
-    console.log(11111111111)
   }
 }
-
 </script>
+
 <style lang="less">
-@import '../assets/css/Es6.less';
-.show {
-  width: 20vw;
-  height: 20vw;
-  background: yellow;
-  position: absolute;
+h1, h2 {
+  font-weight: normal;
 }
 
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+.vue-upload-img-multiple{
+  border:1px red solid;
+}
 </style>
